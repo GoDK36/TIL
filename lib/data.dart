da7ee07a911779b009f74174c612b987e4d67b49
GoDK36 <siinwoo0306@gmail.com>
2019-11-23 14:26:15 +0900

    first commit

---
+++ lib/data.dart
@@ -0,0 +1,13 @@
+List<String> images = [
+  "assets/20150624_190823.jpg",
+  "assets/20150624_191755.jpg",
+  "assets/20150624_192439.jpg",
+  "assets/20150624_192556.jpg",
+];
+
+List<String> title = [
+  "Sunset of Taiwan",
+  "Taiwan Stores",
+  "Red Lights of Taiwan",
+  "Night View of Taiwan",
+];
