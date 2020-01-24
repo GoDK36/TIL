1bc56bb1b59806be1f5c6659f521840ae55a2770
GoDK36 <siinwoo0306@gmail.com>
2020-01-25 00:10:46 +0900

    ATS 수정

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -5,7 +5,16 @@ class AddTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors,
+      color: Color(0xff757575),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0)
+          )
+        ),
+      ),
     );
   }
 }
